2b7dcccf2b2d2550412bf34cb0b56a4ca7ddade8
kasraly <kasraly@gmail.com>
2017-02-13 18:12:34 -0700

    Update to move to other PC

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -48,11 +48,15 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = 'total_payments'
 poi  = "poi"
 features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
+from sklearn.preprocessing import MinMaxScaler
+scale = MinMaxScaler()
+finance_features = scale.fit_transform(finance_features)
 
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
@@ -64,7 +68,10 @@ plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
-
+from sklearn.cluster import KMeans
+cls = KMeans(2)
+cls.fit(finance_features)
+pred = cls.predict(finance_features)
 
 
 
--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -28,15 +28,18 @@ def parseOutText(f):
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
         ### project part 2: comment out the line below
-        words = text_string
+        #words = text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
+        orig = text_string.split()
+        stemmer = SnowballStemmer('english')      
+        stemmed = [stemmer.stem(entry) for entry in orig]
+                
         
-
-
-
+        words = ' '.join(stemmed)
+        
 
     return words
 
